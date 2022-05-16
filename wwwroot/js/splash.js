527dc1a0a3467b6cba4fdea246183648215c36c0
Peter <peterbredell@gmail.com>
2022-05-16 21:48:14 +0200

    commit

---
+++ wwwroot/js/splash.js
@@ -0,0 +1,28 @@
+﻿const header = document.querySelector("nav");
+const sectionOne = document.querySelector(".home-intro");
+const logo = document.querySelector(".logoImg");
+
+const sectionOneOptions = {
+    rootMargin: "-500px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+    entries,
+    sectionOneObserver
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            header.classList.remove("navbar-dark");
+            header.classList.add("navbar-light", "bg-light", "shadow");
+            logo.src = "/img/splash/logoDark.svg";
+        } else {
+            header.classList.remove("navbar-light", "bg-light", "shadow");
+            header.classList.add("navbar-dark");
+            logo.src = "/img/splash/logoLight.svg";
+        }
+    });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
