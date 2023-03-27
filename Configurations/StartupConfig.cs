def54c6ac459a3e543be491a106dbb7fddd94108
Peter <peterbredell@gmail.com>
2023-03-28 00:27:21 +0200

    Update StartupConfig.cs

--- Configurations/StartupConfig.cs
+++ Configurations/StartupConfig.cs
@@ -15,51 +15,53 @@ namespace BugTracksV3.Configurations;
 
 public static class StartupConfig
 {
-    public static void AddStandardServices(this WebApplicationBuilder builder)
-    {
-        builder.Services.AddControllersWithViews();
-    }
+	public static void AddStandardServices(this WebApplicationBuilder builder)
+	{
+		builder.Services.AddControllersWithViews();
+	}
 
-    public static void AddDatabaseServices(this WebApplicationBuilder builder)
-    {
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseNpgsql(DataUtility.GetConnectionString(builder.Configuration), o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
-    }
+	public static void AddDatabaseServices(this WebApplicationBuilder builder)
+	{
+		var connectionString = DataUtility.GetConnectionString(builder.Configuration);
 
-    public static void AddCustomServices(this WebApplicationBuilder builder)
-    {
-        builder.Services.AddScoped<IBTRolesService, BTRolesService>();
-        builder.Services.AddScoped<IBTCompanyInfoService, BTCompanyInfoService>();
-        builder.Services.AddScoped<IBTProjectService, BTProjectService>();
-        builder.Services.AddScoped<IBTTicketService, BTTicketService>();
-        builder.Services.AddScoped<IBTTicketHistoryService, BTTicketHistoryService>();
-        builder.Services.AddScoped<IBTNotificationService, BTNotificationService>();
-        builder.Services.AddScoped<IBTInviteService, BTInviteService>();
-        builder.Services.AddScoped<IBTFileService, BTFileService>();
-        builder.Services.AddScoped<IBTLookupService, BTLookupService>();
-    }
-    public static void AddEmailServices(this WebApplicationBuilder builder)
-    {
-        builder.Services.AddScoped<IEmailSender, BTEmailService>();
-        builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
-    }
+		builder.Services.AddDbContext<ApplicationDbContext>(options =>
+		options.UseNpgsql(connectionString, o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
+	}
 
-    public static void AddAuthServices(this WebApplicationBuilder builder)
-    {
-        var configuration = builder.Configuration;
+	public static void AddCustomServices(this WebApplicationBuilder builder)
+	{
+		builder.Services.AddScoped<IBTRolesService, BTRolesService>();
+		builder.Services.AddScoped<IBTCompanyInfoService, BTCompanyInfoService>();
+		builder.Services.AddScoped<IBTProjectService, BTProjectService>();
+		builder.Services.AddScoped<IBTTicketService, BTTicketService>();
+		builder.Services.AddScoped<IBTTicketHistoryService, BTTicketHistoryService>();
+		builder.Services.AddScoped<IBTNotificationService, BTNotificationService>();
+		builder.Services.AddScoped<IBTInviteService, BTInviteService>();
+		builder.Services.AddScoped<IBTFileService, BTFileService>();
+		builder.Services.AddScoped<IBTLookupService, BTLookupService>();
+	}
+	public static void AddEmailServices(this WebApplicationBuilder builder)
+	{
+		builder.Services.AddScoped<IEmailSender, BTEmailService>();
+		builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+	}
 
-        // Google
-        builder.Services.AddAuthentication()
-            .AddGoogle(googleOptions =>
-            {
-                googleOptions.ClientId = configuration["Google:ClientId"];
-                googleOptions.ClientSecret = configuration["Google:ClientSecret"];
-            });
+	public static void AddAuthServices(this WebApplicationBuilder builder)
+	{
+		var configuration = builder.Configuration;
 
-        builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
-            .AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddClaimsPrincipalFactory<BTUserClaimsPrincipalFactory>()
-            .AddDefaultUI()
-            .AddDefaultTokenProviders();
-    }
+		// Google
+		builder.Services.AddAuthentication()
+			.AddGoogle(googleOptions =>
+			{
+				googleOptions.ClientId = configuration["Google:ClientId"];
+				googleOptions.ClientSecret = configuration["Google:ClientSecret"];
+			});
+
+		builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+			.AddEntityFrameworkStores<ApplicationDbContext>()
+			.AddClaimsPrincipalFactory<BTUserClaimsPrincipalFactory>()
+			.AddDefaultUI()
+			.AddDefaultTokenProviders();
+	}
 }
